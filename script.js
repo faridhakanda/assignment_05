c97501884059d2a4a122b5b3746f3ae3ec675910
faridhakanda <faridakanda1729@gmail.com>
2026-03-08 15:09:04 +0600

    login section design is ok

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("github issue tracker project!");
+// console.log("github issue tracker project!");
+const loginBtn = document.getElementById('login-btn');
+loginBtn.addEventListener('click', () => {
+    const userName = document.getElementById('user-name');
+    const userPassword = document.getElementById('user-password');
+    const user = userName.value;
+    const password = userPassword.value;
+    //console.log(user, password);
+    if (user == "admin" && password == "admin123") {
+        console.log(user, password);
+        console.log("User is authenticated!");
+    } else {
+        alert("Login is failed!");
+    }
+});
+
