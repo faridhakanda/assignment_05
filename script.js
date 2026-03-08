724aeae617fe5bd7d125faf6861c86978e6037f3
faridhakanda <faridakanda1729@gmail.com>
2026-03-08 15:10:31 +0600

    login javascript code is partially ready

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ loginBtn.addEventListener('click', () => {
     const userPassword = document.getElementById('user-password');
     const user = userName.value;
     const password = userPassword.value;
-    //console.log(user, password);
+    // console.log(user, password);
     if (user == "admin" && password == "admin123") {
         console.log(user, password);
         console.log("User is authenticated!");
