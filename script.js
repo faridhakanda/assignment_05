369b2978b14b4d6da603f033872f725bcd24a6d6
faridhakanda <faridakanda1729@gmail.com>
2026-03-08 15:16:06 +0600

    login html and javascript is ready

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-// console.log("github issue tracker project!");
+
+
+
+
+
+
+
+// Login function javascript code
+// for user authentication with 
+// user-name and user-password
 const loginBtn = document.getElementById('login-btn');
 loginBtn.addEventListener('click', () => {
     const userName = document.getElementById('user-name');
